a52425dd88c5d8bd2aface40873cf82664f03749
superjaewoni@gmail.com <sky2blue>
2015-12-16 22:06:23 +0900

    날짜와 날짜수정버튼을 추가하였고, 선택하시에 택스트와 버튼사이에 날짜가 나온다. (살짝 수정 필요)

--- app/src/main/java/asc/com/coy_admin/PlanFragment.java
+++ app/src/main/java/asc/com/coy_admin/PlanFragment.java
@@ -1,5 +1,7 @@
 package asc.com.coy_admin;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,10 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import com.parse.ParseObject;
+
+import java.util.Calendar;
 
 
 /**
@@ -20,6 +27,10 @@ import com.parse.ParseObject;
 public class PlanFragment extends Fragment {
     LinearLayout cur_layout;
     ParseObject tempObject = new ParseObject("Plan");
+    int cur_year;
+    int cur_mon;
+    int cur_day;
+    TextView text_date;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +43,9 @@ public class PlanFragment extends Fragment {
         final EditText editdetail=(EditText)cur_layout.findViewById(R.id.editdetail);
         final EditText editplan=(EditText)cur_layout.findViewById(R.id.editplan);
         Button btAdd=(Button)cur_layout.findViewById(R.id.btAdd);
+        Button btDate = (Button)cur_layout.findViewById(R.id.btDate);
         Button btADD_plan=(Button)cur_layout.findViewById(R.id.addplan);
+        text_date = (TextView)cur_layout.findViewById(R.id.txtDate);
 
         btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +65,24 @@ public class PlanFragment extends Fragment {
                 tempObject.saveInBackground();
             }
         });
+        btDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cur_year= Calendar.getInstance().get(Calendar.YEAR);
+                cur_mon= Calendar.getInstance().get(Calendar.MONTH);
+                cur_day=Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
 
+
+                Dialog date_picker=new DatePickerDialog(getActivity(),new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        text_date.setText(""+cur_year+"."+(cur_mon + 1)+"."+ cur_day);
+                    }
+                },cur_year,cur_mon,cur_day);
+                date_picker.show();
+
+            }
+        });
         return cur_layout;
     }
 
