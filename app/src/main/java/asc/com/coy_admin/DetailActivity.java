5f872151b254b6c1ba1752ce812c3d5093b57ec8
superjaewoni@gmail.com <sky2blue>
2015-12-16 21:05:08 +0900

    수정해서 값 넘겨주는 부분 코딩하는 중이였음

--- app/src/main/java/asc/com/coy_admin/DetailActivity.java
+++ app/src/main/java/asc/com/coy_admin/DetailActivity.java
@@ -1,32 +1,79 @@
 package asc.com.coy_admin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 /**
  * Created by THE KWON on 2015-12-12.
  */
 public class DetailActivity extends AppCompatActivity {
+
+    Intent intent;
+    EditText textSub;
+    EditText textLocation;
+    EditText textIntro;
+    EditText textPhone;
+    EditText editLeader;
+    Button btImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        intent = getIntent();
         Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("detail");
+        getSupportActionBar().setTitle(intent.getStringExtra("tgClubName"));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        TextView textSub = (TextView)findViewById(R.id.textSub);
-        TextView textLocation = (TextView)findViewById(R.id.textLocation);
-        TextView textDetail = (TextView)findViewById(R.id.textDetail);
-        TextView textPhone = (TextView)findViewById(R.id.textPhone);
-        Button btImg = (Button)findViewById(R.id.btImg);
-        EditText editLeader = (EditText)findViewById(R.id.editLeader);
+        textSub = (EditText)findViewById(R.id.textSub);
+        textLocation = (EditText)findViewById(R.id.textLocation);
+        textIntro = (EditText)findViewById(R.id.textDetail);
+        textPhone = (EditText)findViewById(R.id.textPhone);
+        btImg = (Button)findViewById(R.id.btImg);
+        editLeader = (EditText)findViewById(R.id.editLeader);
+
+        textSub.setText(intent.getStringExtra("tgClubSub"));
+        textLocation.setText(intent.getStringExtra("tgClubLotate"));
+        textIntro.setText(intent.getStringExtra("tgClubIntro"));
+        textPhone.setText(intent.getStringExtra("tgClubPhone"));
+        editLeader.setText(intent.getStringExtra("tgClubLeader"));
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
 
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_ok) {
+            ParseQuery<ParseObject> parseObject = ParseQuery.getQuery("club");
+            parseObject.getInBackground(getIntent().getStringExtra("tgObjectId"), new GetCallback<ParseObject>() {
+                @Override
+                public void done(ParseObject parseObject, ParseException e) {
+                    parseObject.put("Club_intro", textIntro);
+                    parseObject.put("Club_leader", editLeader);
+                    parseObject.put("Club_phone", textPhone);
+                    parseObject.put("Club_sub", textSub);
+                    parseObject.saveInBackground();
+                }
+            });
+            }
+        return super.onOptionsItemSelected(item);
+    }
 }
